8417ec21fac99af444ef9853aee739824a578873
Ivan Adamo <ivan.adamo.8@gmail.com>
2022-10-10 21:38:36 -0300

    new file and new folder

---
+++ src/scope/strict.js
@@ -0,0 +1,10 @@
+//'use strict';
+//pi = 3.1416; // no se muestra en consola, es necesario declarar antes de asignar.
+//console.log(pi);
+
+function myFunction() {
+   // 'use strict';
+    return pi = 3.1416; // nuevamente, es necesario declarar antes de asignar.
+}
+
+console.log(myFunction());
